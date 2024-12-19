48b3b3b5e8afd8c707a08b910be56d835f70ee5e
Vinicius Setti <vinicius.setti@trust.group>
2024-12-19 15:51:22 -0300

    criar entidade e repositoria de Customer

--- src/shared/typeorm/migrations/1734633621263-CreateCustomers.ts
+++ src/shared/typeorm/migrations/1734633621263-CreateCustomers.ts
@@ -1,12 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsers1734540285401 implements MigrationInterface {
+export class CreateCustomers1734633621263 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-
     await queryRunner.createTable(
       new Table({
-        name: 'users',
+        name: 'customers',
         columns: [
           {
             name: 'id',
@@ -22,16 +20,6 @@ export class CreateUsers1734540285401 implements MigrationInterface {
           {
             name: 'email',
             type: 'varchar',
-            isUnique: true,
-          },
-          {
-            name: 'password',
-            type: 'varchar',
-          },
-          {
-            name: 'avatar',
-            type: 'varchar',
-            isNullable: true,
           },
           {
             name: 'created_at',
@@ -49,6 +37,6 @@ export class CreateUsers1734540285401 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('users');
+    await queryRunner.dropTable('customers');
   }
 }
